a16215a3b16a76461090117e4ad9123b273b08bb
Jeffrey Toppings <jeffrey.toppings@usask.ca>
2018-12-19 22:14:11 -0600

    Created test script, reorganized folders

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ class Game(object):
 
     def __init__(self, players_list):
         self.players = players_list
+        self.number_of_hands = 60 // len(players_list)
 
     def __str__(self):
         for player in self.players:
