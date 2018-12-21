6158449d4f767e0bf78411be453d100e10ac6191
Jeffrey Toppings <jeffrey.toppings@usask.ca>
2018-12-20 19:09:42 -0600

    main menu of gui, only the exit button works

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ blue = "#275BAD"
 yellow = "#ffd311"
 red = "#c92810"
 
+
 def main_menu():
     window = Tk()
     window.title("Wizard App")
@@ -32,7 +33,7 @@ def main_menu():
 
     # put buttons at bottom
     new_game_button = Button(contents, text="New Game", height="2", width="20")
-    exit_button = Button(contents, text="Exit", height="2", width="20")
+    exit_button = Button(contents, text="Exit", height="2", width="20", command=window.destroy)
 
     new_game_button.grid(row=0, column=0, padx=15, pady=175)
     exit_button.grid(row=0, column=1, padx=15, pady=175)
